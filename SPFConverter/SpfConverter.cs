691df4f41a0d4a3422c76fceb67bed1bb42ff6ec
FallenDev <Paul.Buck@TheBuckNetwork.com>
2023-04-11 22:36:39 -0400

    Added unknown to frameHeader

--- SPFConverter/SpfConverter.cs
+++ SPFConverter/SpfConverter.cs
@@ -56,6 +56,44 @@ namespace SPFConverter
             var frameDataBytes = BitmapToFrameData(loadedBitmap, palette);
             binaryWriter.Write(frameDataBytes);
         }
+
+        private static SpfPalette SpfPaletteFromBitmap(Bitmap bitmap)
+        {
+            // Create an empty SpfPalette struct
+            var spfPalette = new SpfPalette
+            {
+                _alpha = new byte[512],
+                _rgb = new byte[512],
+                _colors = new Color[256]
+            };
+
+            // Extract colors from the Bitmap
+            var colorPalette = bitmap.Palette;
+            var colorCount = colorPalette.Entries.Length;
+
+            // Fill the _colors array with the extracted colors
+            for (var i = 0; i < colorCount; i++)
+                spfPalette._colors[i] = colorPalette.Entries[i];
+
+            // Convert colors to RGB 555 format and store them in the _rgb and _alpha array
+            for (var i = 0; i < colorCount; i++)
+            {
+                var color = spfPalette._colors[i];
+                var red = color.R / 8;
+                var green = color.G / 8;
+                var blue = color.B / 8;
+                var rgb555 = (ushort)((red << 10) | (green << 5) | blue);
+                var rgbBytes = BitConverter.GetBytes(rgb555);
+
+                spfPalette._rgb[i * 2] = rgbBytes[0];
+                spfPalette._rgb[i * 2 + 1] = rgbBytes[1];
+
+                spfPalette._alpha[i * 2] = color.A;
+                spfPalette._alpha[i * 2 + 1] = 0;
+            }
+
+            return spfPalette;
+        }
         
         private static byte[] SpfFileHeaderToBytes(SpfFileHeader header)
         {
@@ -87,11 +125,11 @@ namespace SPFConverter
             spfFrameHeader.PadHeight = 0;
             spfFrameHeader.PixelWidth = (ushort)bitmap.Width;
             spfFrameHeader.PixelHeight = (ushort)bitmap.Height;
-            spfFrameHeader.Unknown = 0;
+            spfFrameHeader.Unknown = 3435973836; // Every SPF has this value associated with it
             spfFrameHeader.Reserved = 0;
-            spfFrameHeader.StartAddress = 0; // Set this value later when you know the correct start address
+            spfFrameHeader.StartAddress = 0; // Have not seen this change
             spfFrameHeader.ByteWidth = (uint)bitmap.Width;
-            spfFrameHeader.SemiByteCount = 0; // You can set this value later if you need it for your specific use case
+            spfFrameHeader.SemiByteCount = 0;
 
             // Write the bytesTotal (bitmap width & bitmap height) *2 if 16bpp
             if (bitmap.PixelFormat == PixelFormat.Format8bppIndexed)
@@ -100,47 +138,12 @@ namespace SPFConverter
                 spfFrameHeader.ByteCount = (uint)(bitmap.Width * bitmap.Height) * 2;
 
             // Convert the SpfFrameHeader struct to bytes
-            var headerBytes = new byte[Marshal.SizeOf(typeof(SpfFrameHeader))];
+            var headerBytes = new byte[Marshal.SizeOf(spfFrameHeader)];
             var gcHandle = GCHandle.Alloc(headerBytes, GCHandleType.Pinned);
             Marshal.StructureToPtr(spfFrameHeader, gcHandle.AddrOfPinnedObject(), false);
             gcHandle.Free();
 
             return headerBytes;
-        }
-
-        private static SpfPalette SpfPaletteFromBitmap(Bitmap bitmap)
-        {
-            // Create an empty SpfPalette struct
-            var spfPalette = new SpfPalette
-            {
-                _alpha = new byte[512],
-                _rgb = new byte[512],
-                _colors = new Color[256]
-            };
-
-            // Extract colors from the Bitmap
-            var colorPalette = bitmap.Palette;
-            var colorCount = colorPalette.Entries.Length;
-
-            // Fill the _colors array with the extracted colors
-            for (var i = 0; i < colorCount; i++)
-                spfPalette._colors[i] = colorPalette.Entries[i];
-
-            // Convert colors to RGB 555 format and store them in the _rgb array
-            for (var i = 0; i < colorCount; i++)
-            {
-                var color = spfPalette._colors[i];
-                var red = color.R / 8;
-                var green = color.G / 8;
-                var blue = color.B / 8;
-                var rgb555 = (ushort)((red << 10) | (green << 5) | blue);
-                var rgbBytes = BitConverter.GetBytes(rgb555);
-
-                spfPalette._rgb[i * 2] = rgbBytes[0];
-                spfPalette._rgb[i * 2 + 1] = rgbBytes[1];
-            }
-
-            return spfPalette;
         }
 
         private static byte[] BitmapToFrameData(Bitmap bitmap, SpfPalette spfPalette)
@@ -155,7 +158,7 @@ namespace SPFConverter
             frameHeader.Reserved = 0;
             frameHeader.StartAddress = 0; // Set this value later when you know the correct start address
             frameHeader.ByteWidth = (uint)bitmap.Width;
-            frameHeader.SemiByteCount = 0; // You can set this value later if you need it for your specific use case
+            frameHeader.SemiByteCount = 0;
 
             // Write the bytesTotal (bitmap width & bitmap height) *2 if 16bpp
             if (bitmap.PixelFormat == PixelFormat.Format8bppIndexed)
